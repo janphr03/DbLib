5b62d62a6a56d188eaeb6b270fb58522f7e89ea0
janphr03 <159697326+janphr03@users.noreply.github.com>
2024-11-11 12:40:31 +0100

    Logger mit Interface implementiert

---
+++ App/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public interface ILogger
+    {
+        void Trace(string message);
+        void Debug(string message);
+        void Info(string message);
+        void Error(string message, Exception ex = null);
+    }
+
+}
